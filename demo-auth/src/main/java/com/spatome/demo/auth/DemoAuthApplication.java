13f2aab98362d51f29741cec71cfe03863690d39
spatome <spatome@126.com>
2018-10-24 18:53:26 +0800

    第一次

---
+++ demo-auth/src/main/java/com/spatome/demo/auth/DemoAuthApplication.java
@@ -0,0 +1,12 @@
+package com.spatome.demo.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthApplication.class, args);
+	}
+}
