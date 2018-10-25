414e042a9e40b826c8d766f337431cbe2a1fbb5b
spatome <spatome@126.com>
2018-10-25 16:22:47 +0800

    第一次

--- demo-cloud-config/src/main/java/com/spatome/demo/config/DemoCloudConfigApplication.java
+++ demo-cloud-config/src/main/java/com/spatome/demo/config/DemoCloudConfigApplication.java
@@ -3,6 +3,9 @@ package com.spatome.demo.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -12,4 +15,13 @@ public class DemoCloudConfigApplication {
 		SpringApplication.run(DemoCloudConfigApplication.class, args);
 	}
 
+    @Configuration
+    public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+        	http.csrf().disable();
+        	super.configure(http);
+        }
+    }
 }
